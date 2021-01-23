55a2a0ea6f2cbbb0e514b44dd1fb8e246f5b06e3
YanaiChiharu <yanaichiharu@MacBook-Air-3.local>
2021-01-23 12:04:58 +0900

    create best_sol func

--- Firefly_mra/firefly_algorithm.py
+++ Firefly_mra/firefly_algorithm.py
@@ -50,8 +50,9 @@ def valueF(pos):
 
 
 # ホタルの移動 =========================================================
-# ホタルの数
-firefly = 30
+# ホタルの数とその行列
+firefly = 4
+firefly_matrix = np.zeros((firefly, N))
 
 # 初期位置(解候補の行列)
 X = np.zeros((firefly, N))
@@ -76,18 +77,83 @@ def attract(Fi, Fj):
     
     return beta0 * pow(e, - (gamma * d**2))
 
-a = np.array([1, 2, 3])
-b = np.array([4, 5, 6])
-print(attract(a, b))
 
-def update_x(xi, Fi, Fj):
-    x = np.zeros(N)
+def move_x(Fi, Fj):
     alpha = 0.5
-    epsv = np.zeros(N)
-    
-    for i in range(N):
-        epsv[i] = rd.uniform(-0.5, 0.5)
+    epsv = rd.uniform(-0.5, 0.5)
 
-    return xi + attract(Fi, Fj) * (Fj - Fi) + alpha * epsv
+    return Fi + attract(Fi, Fj) * (Fj - Fi) + alpha * epsv
         
+# 生成し置換する
+def change(X, firefly_matrix):
+    Fi = np.zeros(N)
+    Fj = np.zeros(N)
+        
+    # Fi が Fj より明るかったら移動
+    for i in range(0, firefly, 2):
+        for j in range(N):
+            Fi[j] = X[i][j]
+            Fj[j] = X[i+1][j]
+            
+        if (valueF(Fi) < valueF(Fj)):
+            # Fi の代わりに firefly_matrix の半分を更新する
+            for j in range(N):
+                firefly_matrix[i][j] = move_x(Fi[j], Fj[j])
 
+        else:
+            for j in range(N):
+                firefly_matrix[i][j] = X[i][j]
+        '''
+        for j in range(N):
+            firefly_matrix[i+1][j] = X[i+1][j]
+        '''   
+
+    return firefly_matrix
+
+# firefly_matrix = change(X, firefly_matrix)
+
+# X を改善
+def recreate_x(X, firefly_matrix):
+    X = firefly_matrix
+
+    for i in range(1, firefly, 2):
+        for j in range(N):
+            X[i][j] = np.array([rd.uniform(-1, 1)])
+
+    return X
+            
+
+# 最前解を記録する
+def best_sol(matrix, pos):
+    pos_tmp = np.zeros(N)
+
+    for i in range(firefly):
+        for j in range(N):
+            pos_tmp[j] = matrix[i][j]
+
+        if (valueF(pos) > valueF(pos_tmp)):
+            pos = pos_tmp
+        
+    return pos
+    
+            
+
+if __name__ == '__main__':
+
+    # 終了条件
+    eps = pow(10, -5)
+
+    # 最大反復回数
+    max_generarion = 1000
+
+    
+    for generation in range(max_generarion):
+        firefly_matrix = change(X, firefly_matrix)
+
+        X = recreate_x(X, firefly_matrix)
+
+        pos = best_sol(firefly_matrix, pos)
+
+        
+    
+    
